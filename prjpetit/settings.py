a7907bb5f192ab21d25bc257363006eae83b70d2
unknown <santifigu@users.noreply.github.com>
2026-06-04 18:17:13 -0300

    arreglo de host

--- prjpetit/settings.py
+++ prjpetit/settings.py
@@ -22,7 +22,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['veterinaria-petit-production.up.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'veterinaria-petit-production.up.railway.app',
+    '.up.railway.app',
+    'localhost',
+    '127.0.0.1',
+]
 
 # Application definition
 
