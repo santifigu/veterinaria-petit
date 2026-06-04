371878b3e0434e852af359bf43ecefecf2c97235
unknown <santifigu@users.noreply.github.com>
2026-06-04 17:20:08 -0300

    Verificar BBDD

--- prjpetit/settings.py
+++ prjpetit/settings.py
@@ -13,7 +13,6 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
