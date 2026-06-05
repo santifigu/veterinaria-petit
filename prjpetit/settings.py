da9458c2ff36ab6b3fe25a89303fcbe9307699ff
unknown <santifigu@users.noreply.github.com>
2026-06-04 23:43:23 -0300

    cambios en los csrf confiables

--- prjpetit/settings.py
+++ prjpetit/settings.py
@@ -91,7 +91,8 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # Configuración de CSRF para no tener problemas en producción con dominios y subdominios
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
+csrf_origins = os.getenv('CSRF_TRUSTED_ORIGINS', '')
+CSRF_TRUSTED_ORIGINS = [x for x in csrf_origins.split(',') if x]
 
 # Bloquear después de 5 intentos fallidos
 AXES_FAILURE_LIMIT = 5 # Número de intentos fallidos antes de bloquear
