b90e468e39e9db859a7874c050e6365ca6e32632
unknown <santifigu@users.noreply.github.com>
2026-06-07 11:40:06 -0300

    Arreglos en menú hamburguesa

--- prjpetit/settings.py
+++ prjpetit/settings.py
@@ -202,3 +202,17 @@ MESSAGES_TAGS = {
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
